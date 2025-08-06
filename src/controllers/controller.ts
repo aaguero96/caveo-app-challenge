b29cfe237f366185ae202e18f9a97d10933edd20
aaguero96 <andre.luiz_1996@hotmail.com>
2025-08-06 00:15:25 -0300

    feat: implement service into controllers

--- src/controllers/controller.ts
+++ src/controllers/controller.ts
@@ -1,26 +1,61 @@
-import { ParameterizedContext } from 'koa';
+import { ParameterizedContext, DefaultContext, DefaultState } from 'koa';
 import { IController } from './controller.interface';
+import { IService } from '../services/service.interface';
+import {
+  EditAccountRequestDto,
+  EditAccountResponseDto,
+  GetMeResponseDto,
+  GetUsersQueryDto,
+  GetUsersResponseDto,
+  SignInOrRegisterRequestDto,
+  SignInOrRegisterResponseDto,
+} from '../dtos';
+import { UserState } from '../states/user.state';
 
-export const createController = (): IController => {
-  return new Controller();
+export const createController = (service: IService): IController => {
+  return new Controller(service);
 };
 
 class Controller implements IController {
-  constructor() {}
+  constructor(private readonly _service: IService) {}
 
-  signInOrRegister = async (ctx: ParameterizedContext): Promise<void> => {
-    throw new Error('Method not implemented.');
+  signInOrRegister = async (
+    ctx: ParameterizedContext<
+      DefaultState,
+      DefaultContext & { request: { body: SignInOrRegisterRequestDto } },
+      SignInOrRegisterResponseDto
+    >,
+  ): Promise<void> => {
+    ctx.status = 201;
+    ctx.body = await this._service.signInOrRegister(ctx.request.body);
   };
 
-  editAccount = async (ctx: ParameterizedContext): Promise<void> => {
-    throw new Error('Method not implemented.');
+  editAccount = async (
+    ctx: ParameterizedContext<
+      UserState,
+      DefaultContext & { request: { body: EditAccountRequestDto } },
+      EditAccountResponseDto
+    >,
+  ): Promise<void> => {
+    ctx.status = 200;
+    ctx.body = await this._service.editAccount(ctx.request.body);
   };
 
-  getMe = async (ctx: ParameterizedContext): Promise<void> => {
-    throw new Error('Method not implemented.');
+  getMe = async (
+    ctx: ParameterizedContext<UserState, DefaultContext, GetMeResponseDto>,
+  ): Promise<void> => {
+    ctx.status = 200;
+    ctx.body = await this._service.getMe(ctx.state.id);
   };
 
-  getUsers = async (ctx: ParameterizedContext): Promise<void> => {
-    throw new Error('Method not implemented.');
+  getUsers = async (
+    ctx: ParameterizedContext<
+      UserState,
+      DefaultContext & { query: GetUsersQueryDto },
+      GetUsersResponseDto
+    >,
+  ): Promise<void> => {
+    ctx.status = 200;
+    ctx.body = await this._service.getUsers(ctx.query);
   };
 }
