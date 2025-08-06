5eb7950654872c5be521e772f5e8982006cc9c28
aaguero96 <andre.luiz_1996@hotmail.com>
2025-08-06 09:04:42 -0300

    feat: update app with services

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,25 @@
 import Koa from 'koa';
 import { createRouter } from './router/router';
 import { createController } from './controllers/controller';
+import { createService } from './services/service';
+import { createSignInOrRegosterService } from './services/sign-in-or-register/sign-in-or-register.service';
+import { createEditAccountService } from './services/edit-account/edit-account.service';
+import { createGetMeService } from './services/get-me/get-me.service';
+import { createGetUsersService } from './services/get-users/get-users.service';
 
 const main = async () => {
-  const controller = createController();
+  const signInOrRegisterService = createSignInOrRegosterService();
+  const editAccountService = createEditAccountService();
+  const getMeService = createGetMeService();
+  const getUsersService = createGetUsersService();
+  const service = createService(
+    signInOrRegisterService,
+    editAccountService,
+    getMeService,
+    getUsersService,
+  );
+
+  const controller = createController(service);
 
   const router = createRouter(controller);
 
